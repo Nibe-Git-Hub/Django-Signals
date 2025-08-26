9d8a9aef2faab702a0a45d8d55f1e3d565dc5596
Nibe <neabella2@student.hau.edu.ph>
2025-08-26 12:58:26 +0800

    Defined Tweet Model

--- Tweets/models.py
+++ Tweets/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Tweet by {self.user.username} on {self.created_at.strftime("%Y-%m-%d")}'
