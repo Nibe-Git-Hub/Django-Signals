0c35c06d7c115b3c068073ae849226e685d06333
Nibe <neabella2@student.hau.edu.ph>
2025-08-26 13:13:33 +0800

    Update urls.py

--- Core/urls.py
+++ Core/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tweet/', include('Tweet.urls')),
+    path('tweet/', include('Tweets.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
