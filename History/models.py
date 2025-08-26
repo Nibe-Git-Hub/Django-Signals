1de4cc370f99d1703b370c1ad07b53f729d8d374
Nibe <neabella2@student.hau.edu.ph>
2025-08-26 13:00:07 +0800

    Defined History Model

--- History/models.py
+++ History/models.py
@@ -3,3 +3,13 @@ from django.contrib.auth.models import User
 from Tweet.models import Tweet
 
 # Create your models here.
+
+class History(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    method = models.CharField(max_length=10) # e.g., 'POST'
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    summary = models.TextField()
+
+    def __str__(self):
+        return self.summary
